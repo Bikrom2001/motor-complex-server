f7573ef1f24edb6bbac2c01ec4f04ed2eed0ba8a
Bikrom-Roy <bikromroy2001@gmail.com>
2022-11-27 00:50:18 +0600

    my product delete

--- index.js
+++ index.js
@@ -168,6 +168,13 @@ async function run() {
             const product = await productsCollection.find(query).toArray();
             res.send(product);
         });
+
+        app.delete('/product/:id', verifyJWT, async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const result = await productsCollection.deleteOne(filter);
+            res.send(result);
+        })
         
         
 
